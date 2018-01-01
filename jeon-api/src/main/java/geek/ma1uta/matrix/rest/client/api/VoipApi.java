23a666e80610bce5376c297ec9799b678d4d6e32
Anatoliy Sablin <sablintolya@gmail.com>
2018-01-01 22:33:51 +0300

    Voip Api and move message and event type to event class.

---
+++ jeon-api/src/main/java/geek/ma1uta/matrix/rest/client/api/VoipApi.java
@@ -0,0 +1,14 @@
+package geek.ma1uta.matrix.rest.client.api;
+
+import geek.ma1uta.matrix.rest.client.model.voip.VoipResponse;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/_matrix/client/r0/voip")
+public interface VoipApi {
+
+    @GET
+    @Path("/turnServer")
+    VoipResponse turnServer();
+}
