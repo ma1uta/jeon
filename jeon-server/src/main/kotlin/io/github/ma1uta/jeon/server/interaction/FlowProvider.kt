076f7e86fc9cddc958faec9852b37e8d4db2aaa0
Anatoliy Sablin <sablintolya@gmail.com>
2018-05-19 14:07:28 +0300

    Renamed jeon to homeserver

--- jeon-server/src/main/kotlin/io/github/ma1uta/jeon/server/interaction/FlowProvider.kt
+++
@@ -1,3 +0,0 @@
-package io.github.ma1uta.jeon.server.interaction
-
-class FlowProvider(val stages: List<StageProvider>)
--- jeon-server/src/main/kotlin/io/github/ma1uta/jeon/server/interaction/PasswordConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package io.github.ma1uta.jeon.server.interaction
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ConditionalOnProperty(name = ["jeon.auth.password.enable"], havingValue = "true")
-class PasswordConfiguration
