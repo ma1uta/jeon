6ea265717bcafaee0efb2789791e0d818fd707fe
Anatoliy Sablin <sablintolya@gmail.com>
2018-06-18 23:24:34 +0300

    Added swagger annotations to the fallback auth api.

--- client-api/src/main/java/io/github/ma1uta/matrix/client/api/FallbackAuthApi.java
+++ client-api/src/main/java/io/github/ma1uta/matrix/client/api/FallbackAuthApi.java
@@ -16,6 +16,14 @@
 
 package io.github.ma1uta.matrix.client.api;
 
+import static io.github.ma1uta.matrix.client.api.FallbackAuthApi.PATH;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
@@ -31,9 +39,15 @@ import javax.ws.rs.core.MediaType;
  * <p/>
  * <a href="https://matrix.org/docs/spec/client_server/r0.3.0.html#login-fallback">Specification.</a>
  */
-@Path("/_matrix")
+@Api(value = PATH, description = "If a client does not recognize any or all login flows it can use the fallback login API.")
+@Path(PATH)
 @Produces(MediaType.TEXT_HTML)
 public interface FallbackAuthApi {
+
+    /**
+     * Fallback auth api url.
+     */
+    String PATH = "/_matrix";
 
     /**
      * Fallback login.
@@ -41,6 +55,10 @@ public interface FallbackAuthApi {
      * @return This returns an HTML and JavaScript page which can perform the entire login process. The page will attempt to call the
      *     JavaScript function window.onLogin when login has been successfully completed.
      */
+    @ApiOperation(value = "Fallback login.", response = String.class)
+    @ApiResponses( {
+        @ApiResponse(code = 200, message = "Login page for the fallback login.")
+    })
     @GET
     @Path("/static/client/login")
     String login();
@@ -50,15 +68,25 @@ public interface FallbackAuthApi {
      * handle a given login type, it can direct the user to a web browser with the URL of a fallback page which will allow the
      * user to complete that login step out-of-band in their web browser.
      *
-     * @param auth            is the type name of the stage it is attempting.
+     * @param auth            The type name of the stage it is attempting.
      * @param session         the ID of the session given by the homeserver.
      * @param servletRequest  servlet request.
      * @param servletResponse servlet response.
      * @return an HTML page which can perform this authentication stage. This page must use the following JavaScript when the
      *     authentication has been completed.
      */
+    @ApiOperation(value = "Fallback login endpoint",
+        notes = "If a client does not know how to handle a given login type, it can direct the user to a web browser with the "
+            + "URL of a fallback page which will allow the user to complete that login step out-of-band in their web browser.",
+        response = String.class)
+    @ApiResponses( {
+        @ApiResponse(code = 200, message = "n HTML page which can perform this authentication stage. This page must use the "
+            + "following JavaScript when the authentication has been completed.")
+    })
     @GET
     @Path("/client/r0/auth/{auth}/fallback/web")
-    String auth(@PathParam("auth") String auth, @QueryParam("session") String session, @Context HttpServletRequest servletRequest,
-                @Context HttpServletResponse servletResponse);
+    String auth(
+        @ApiParam("The type name of the stage it is attempting.") @PathParam("auth") String auth,
+        @ApiParam("the ID of the session given by the homeserver.") @QueryParam("session") String session,
+        @Context HttpServletRequest servletRequest, @Context HttpServletResponse servletResponse);
 }
