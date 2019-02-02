b266047668caa00eac1830a388cf09be6c1ce608
Anatoly Sablin <sablintolya@gmail.com>
2019-02-02 13:42:21 +0300

    Fix Push API path.

--- push-api/src/main/java/io/github/ma1uta/matrix/push/api/PushApi.java
+++ push-api/src/main/java/io/github/ma1uta/matrix/push/api/PushApi.java
@@ -39,7 +39,7 @@ import javax.ws.rs.core.UriInfo;
  * This describes the format used by "HTTP" pushers to send notifications of events to Push Gateways. If the endpoint returns an HTTP
  * error code, the homeserver SHOULD retry for a reasonable amount of time using exponential backoff.
  */
-@Path("/_matrix/push/r0")
+@Path("/_matrix/push/v1")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface PushApi {
