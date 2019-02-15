7b8b36da6d7754d591e258abae49db21d378d9a7
Anatoly Sablin <sablintolya@gmail.com>
2019-02-15 20:43:09 +0300

    Dump API versions. Add methods to get account_data.

--- client-api/src/main/java/io/github/ma1uta/matrix/client/api/ClientConfigApi.java
+++ client-api/src/main/java/io/github/ma1uta/matrix/client/api/ClientConfigApi.java
@@ -29,6 +29,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 import java.util.Map;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -48,6 +49,66 @@ import javax.ws.rs.core.UriInfo;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public interface ClientConfigApi {
+
+    /**
+     * Get some account_data for the client. This config is only visible to the user that set the account_data.
+     * <br>
+     * <b>Requires auth</b>: Yes.
+     * <br>
+     * Return: {@link Map}.
+     * <p>Status code 200: The account data content for the given type.</p>
+     *
+     * @param userId          Required. The id of the user to set account_data for. The access token must be authorized to make
+     *                        requests for this user id.
+     * @param type            Required. The event type of the account_data to set. Custom types should be namespaced to avoid clashes.
+     * @param uriInfo         Request Information.
+     * @param httpHeaders     Http headers.
+     * @param asyncResponse   Asynchronous response.
+     * @param securityContext Security context.
+     */
+    @Operation(
+        summary = "Set some account_data for the client.",
+        description = "This config is only visible to the user that set the account_data. The config will be "
+            + "synced to clients in the top-level account_data.",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "The account_data was successfully added.",
+                content = @Content(
+                    schema = @Schema(
+                        implementation = Map.class
+                    )
+                )
+            )
+        },
+        security = {
+            @SecurityRequirement(
+                name = "accessToken"
+            )
+        },
+        tags = {
+            "User data"
+        }
+    )
+    @GET
+    @Secured
+    @Path("/{userId}/account_data/{type}")
+    void getConfig(
+        @Parameter(
+            description = "The id of the user to get account_data for. The access token must be authorized to make requests"
+                + " for this user id.",
+            required = true
+        ) @PathParam("userId") Id userId,
+        @Parameter(
+            description = "The event type of the account_data to get. Custom types should be namespaced to avoid clashes.",
+            required = true
+        ) @PathParam("type") String type,
+
+        @Context UriInfo uriInfo,
+        @Context HttpHeaders httpHeaders,
+        @Suspended AsyncResponse asyncResponse,
+        @Context SecurityContext securityContext
+    );
 
     /**
      * Set some account_data for the client. This config is only visible to the user that set the account_data. The config will be
@@ -107,6 +168,71 @@ public interface ClientConfigApi {
         @RequestBody(
             description = "Account data"
         ) Map<String, Object> accountData,
+
+        @Context UriInfo uriInfo,
+        @Context HttpHeaders httpHeaders,
+        @Suspended AsyncResponse asyncResponse,
+        @Context SecurityContext securityContext
+    );
+
+    /**
+     * Get some account_data for the client on a given room. This config is only visible to the user that set the account_data.
+     * <br>
+     * <b>Requires auth</b>: Yes.
+     * <br>
+     * Return: {@link Map}.
+     * <p>Status code 200: The account data content for the given type.</p>
+     *
+     * @param userId          Required. The id of the user to set account_data for. The access token must be authorized to make requests for
+     *                        this user id.
+     * @param roomId          Required. The id of the room to set account_data on.
+     * @param type            Required. The event type of the account_data to set. Custom types should be namespaced to avoid clashes.
+     * @param uriInfo         Request Information.
+     * @param httpHeaders     Http headers.
+     * @param asyncResponse   Asynchronous response.
+     * @param securityContext Security context.
+     */
+    @Operation(
+        summary = "Set some account_data for the client on a given room.",
+        description = "This config is only visible to the user that set the account_data. The config will be synced to clients in the "
+            + "per-room account_data.",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "The account_data was successfully added.",
+                content = @Content(
+                    schema = @Schema(
+                        implementation = Map.class
+                    )
+                )
+            )
+        },
+        security = {
+            @SecurityRequirement(
+                name = "accessToken"
+            )
+        },
+        tags = {
+            "User data"
+        }
+    )
+    @GET
+    @Secured
+    @Path("/{userId}/rooms/{roomId}/account_data/{type}")
+    void getRoomConfig(
+        @Parameter(
+            description = "The id of the user to get account_data for. The access token must be authorized to make requests"
+                + " for this user id.",
+            required = true
+        ) @PathParam("userId") Id userId,
+        @Parameter(
+            description = "The id of the room to get account_data on.",
+            required = true
+        ) @PathParam("roomId") Id roomId,
+        @Parameter(
+            description = "The event type of the account_data to set. Custom types should be namespaced to avoid clashes.",
+            required = true
+        ) @PathParam("type") String type,
 
         @Context UriInfo uriInfo,
         @Context HttpHeaders httpHeaders,
